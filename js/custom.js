f0e9b3642b396ca1753d546e6c7ac9c92dea871c
dimarionss <dimarionss@gmail.com>
2020-06-12 19:28:14 +0300

    почти завершение сайта

--- js/custom.js
+++ js/custom.js
@@ -9,44 +9,16 @@ $(document).ready(function () {
     });
 });
 
+function stateChange() {
+    var obj = document.getElementById('dropdown');
 
-// function DropDown(el) {
-//     this.dd = el;
-//     this.placeholder = this.dd.children('span');
-//     this.opts = this.dd.find('ul.dropdown > li');
-//     this.val = '';
-//     this.index = -1;
-//     this.initEvents();
-// }
-// DropDown.prototype = {
-//     initEvents: function () {
-//         var obj = this;
-//         obj.dd.on('click', function (event) {
-//             $(this).toggleClass('active');
-//             return false;
-//         });
-//         obj.opts.on('click', function () {
-//             var opt = $(this);
-//             obj.val = opt.text();
-//             obj.index = opt.index();
-//             obj.placeholder.text(obj.val);
-//         });
-//     },
-//     getValue: function () {
-//         return this.val;
-//     },
-//     getIndex: function () {
-//         return this.index;
-//     }
-// }
-// $(function () {
-//     var dd = new DropDown($('#dd'));
-//     $(document).click(function () {
-//         $('.wrapper-dropdown-3').removeClass('active');
-//     });
-// });
+    obj.style.display =
+        (obj.style.display == "none" ?
+            "flex" : "none");
+}
+
 
 function check2() {
     var url = $('input[name=radio]:checked').val();
-    $(location).attr('href', url);
+    $(location).prop('href', url);
 }
